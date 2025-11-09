34a92f7a84fbc1db904f44c2e45e8ecf53bf7599
Jordan Kay <jordan@fleuronic.com>
2025-11-09 13:04:03 -0500

    Don’t use separators in menu container

--- WorkflowContainers/Sources/Menu/MenuContainer.swift
+++ WorkflowContainers/Sources/Menu/MenuContainer.swift
@@ -3,56 +3,52 @@
 #if canImport(AppKit)
 
 import AppKit
-import WorkflowMenuUI
 import ViewEnvironment
+import WorkflowMenuUI
 
-public extension Menu {
-	final class Container<Content: WorkflowMenuUI.Screen>: ScreenMenu<Menu.Screen<Content>> {
-		private let superDelegate: MenuDelegate
-		private var menus: [AnyHashable: NSMenu] = [:]
-		private var separatorItems: [AnyHashable: NSMenuItem] = [:]
+extension Menu {
+    public final class Container<Content: WorkflowMenuUI.Screen>: ScreenMenu<Menu.Screen<Content>> {
+        private let superDelegate: MenuDelegate
+        private var menus: [AnyHashable: NSMenu] = [:]
 
-		public required init(screen: Menu.Screen<Content>, environment: ViewEnvironment) {
-			superDelegate = MenuDelegate()
-			super.init(screen: screen, environment: environment)
-			delegate = superDelegate
-		}
+        public required init(screen: Menu.Screen<Content>, environment: ViewEnvironment) {
+            self.superDelegate = MenuDelegate()
+            super.init(screen: screen, environment: environment)
+            delegate = superDelegate
+        }
 
-		// MARK: ScreenMenu
-		override public func screenDidChange(from previousScreen: Menu.Screen<Content>, previousEnvironment: ViewEnvironment) {
-			super.screenDidChange(from: previousScreen, previousEnvironment: previousEnvironment)
+        // MARK: ScreenMenu
 
-			let keys = screen.sections.map(\.key)
-			let previousKeys = previousScreen.sections.map(\.key)
-			let removedKeys = previousKeys.filter { !keys.contains($0) }
-			removedKeys.forEach { menus.removeValue(forKey: $0) }
+        override public func screenDidChange(from previousScreen: Menu.Screen<Content>, previousEnvironment: ViewEnvironment) {
+            super.screenDidChange(from: previousScreen, previousEnvironment: previousEnvironment)
 
-			let items = screen.sections.flatMap { section in
-				let previousMenu = menus[section.key]
-				let menu = previousMenu ?? {
-					let menu = section.screen.buildMenu(in: environment)
-					superDelegate.subDelegates[section.key] = menu.delegate
-					menus[section.key] = menu
-					return menu
-				}()
+            let keys = screen.sections.map(\.key)
+            let previousKeys = previousScreen.sections.map(\.key)
+            let removedKeys = previousKeys.filter { !keys.contains($0) }
+            removedKeys.forEach { menus.removeValue(forKey: $0) }
 
-				if previousMenu != nil {
-					section.screen.update(menu: menu, with: environment)
-				}
+            let items = screen.sections.flatMap { section in
+                let previousMenu = menus[section.key]
+                let menu = previousMenu ?? {
+                    let menu = section.screen.buildMenu(in: environment)
+                    superDelegate.subDelegates[section.key] = menu.delegate
+                    menus[section.key] = menu
+                    return menu
+                }()
 
-				return menu.items + [separatorItems[section.key] ?? {
-					let item = NSMenuItem.separator()
-					separatorItems[section.key] = item
-					return item
-				}()]
-			}
+                if previousMenu != nil {
+                    section.screen.update(menu: menu, with: environment)
+                }
 
-			if self.items != items {
-				items.forEach { $0.menu = nil }
-				self.items = items
-			}
-		}
-	}
+                return menu.items
+            }
+
+            if self.items != items {
+                items.forEach { $0.menu = nil }
+                self.items = items
+            }
+        }
+    }
 }
 
 #endif
